16f4c6af15184d7490c17bf75c9a691f0403b1a2
czl531 <531040183@qq.com>
2025-12-19 20:23:36 +0800

    fix

--- autopcr/model/custom.py
+++ autopcr/model/custom.py
@@ -243,6 +243,14 @@ class TalentQuestData(BaseModel):
     talent_id: int = None
     clear_count: int = None
 
+
+class EffectiveUnitData(BaseModel):
+    unit_id: int = 0
+    unit_lv: int = 0
+    rank: int = 0
+    equip_num: int = 0
+    equip_enhance_num: int = 0
+
 class eDifficulty(IntEnum):
     NONE = 0
     NORMAL = 1
